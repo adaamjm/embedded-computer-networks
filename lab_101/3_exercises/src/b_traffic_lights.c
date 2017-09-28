ecb0f40f6de894b11148fae9dde301eb3c0a5f76
adaamjm <adaamjm@users.noreply.github.com>
2017-09-28 21:16:42 +0100

    Task Updates
    Finsished traffic lights task.
    
    LED dice also done but need to add button input

--- lab_101/3_exercises/src/b_traffic_lights.c
+++ lab_101/3_exercises/src/b_traffic_lights.c
@@ -13,8 +13,15 @@
 #include "clock.h"
 #include "gpio.h"
 
-// map the led to GPIO PA8
-gpio_pin_t led = {PI_1, GPIOI, GPIO_PIN_1};
+// TL_SET1: map the leds to GPIOs
+gpio_pin_t led0 = {PI_1, GPIOI, GPIO_PIN_1};		//	RED
+gpio_pin_t led1 = {PB_14, GPIOB, GPIO_PIN_14};	//	ORANGE
+gpio_pin_t led2 = {PB_15, GPIOB, GPIO_PIN_15};	//	GREEN
+
+// TL_SET2: map the leds to GPIOs
+gpio_pin_t led3 = {PI_2, GPIOI, GPIO_PIN_2};		//	RED
+gpio_pin_t led4 = {PA_15, GPIOA, GPIO_PIN_15};	//	ORANGE
+gpio_pin_t led5 = {PA_8, GPIOA, GPIO_PIN_8};	//	GREEN
 
 // this is the main method
 int main()
@@ -25,15 +32,48 @@ int main()
   init_sysclk_216MHz();
   
   // initialise the gpio pin
-  init_gpio(led, OUTPUT);
+  init_gpio(led0, OUTPUT);
+	init_gpio(led1, OUTPUT);
+	init_gpio(led2, OUTPUT);
+	init_gpio(led3, OUTPUT);
+	init_gpio(led4, OUTPUT);
+	init_gpio(led5, OUTPUT);
   
   // loop forever ...
   while(1)
   {
-    // toggle the led on the gpio pin
-    toggle_gpio(led);
-    
-    // wait for 1 second
-    HAL_Delay(1000);
+		//	PHASE 1
+		toggle_gpio(led0);	//	TL_SET1: RED ON
+		toggle_gpio(led5);	//	TL_SET2: GREEN ON
+		HAL_Delay(2000);		//	WAIT 2 SECONDS
+		
+		//	PHASE 2
+		toggle_gpio(led4);	//	TL_SET2: ORANGE ON
+		toggle_gpio(led5);	//	TL_SET2: GREEN OFF
+		HAL_Delay(2000);		//	WAIT 2 SECONDS
+		
+		//	PHASE 3
+		toggle_gpio(led4);	//	TL_SET2: ORANGE OFF
+		toggle_gpio(led3);	//	TL_SET2: RED ON
+		HAL_Delay(2000);		//	WAIT 2 SECONDS
+		
+		//	PHASE 4
+		toggle_gpio(led2);	//	TL_SET1: GREEN ON
+		toggle_gpio(led0);	//	TL_SET1: RED OFF
+		HAL_Delay(2000);		//	WAIT 2 SECONDS
+
+		//	PHASE 5
+		toggle_gpio(led2);	//	TL_SET1: GREEN OFF
+		toggle_gpio(led1);	//	TL_SET1: ORANGE ON
+		HAL_Delay(2000);		//	WAIT 2 SECONDS
+		
+		//	PHASE 6
+		toggle_gpio(led0);	//	TL_SET1: RED ON
+		toggle_gpio(led1);	//	TL_SET1: ORANGE OFF
+		HAL_Delay(2000);		//	WAIT 2 SECONDS
+		
+		//	RESET LEDS
+		toggle_gpio(led0);	//	TL_SET1: RED OFF
+		toggle_gpio(led3);	//	TL_SET2: RED OFF
   }
 }
--- lab_101/3_exercises/src/c_led_dice.c
+++ lab_101/3_exercises/src/c_led_dice.c
@@ -1,5 +1,5 @@
 /*
- * c_traffic_lights.c
+ * b_traffic_lights.c
  *
  * this is the main application skeleton for the student exercises in task 3
  *
@@ -12,9 +12,35 @@
 #include "pinmappings.h"
 #include "clock.h"
 #include "gpio.h"
+#include "random_numbers.h"
 
-// map the led to GPIO PA8
-gpio_pin_t led = {PI_1, GPIOI, GPIO_PIN_1};
+/*
+*				A						B
+*				
+*				C			D			E
+*
+*				F						G
+*
+*				ONE		= D
+*				TWO		= A, G
+* 			THREE	= A, D, G
+* 			FOUR	= A, B, F, G
+* 			FIVE	= A, B, D, F, G
+*				SIX		= A, B, C, E, F, G
+*/
+
+
+gpio_pin_t led0 = {PI_1, GPIOI, GPIO_PIN_1};		//	A
+gpio_pin_t led1 = {PB_14, GPIOB, GPIO_PIN_14};	//	B
+gpio_pin_t led2 = {PB_15, GPIOB, GPIO_PIN_15};	//	C
+gpio_pin_t led3 = {PB_9, GPIOB, GPIO_PIN_9};		//	D
+gpio_pin_t led4 = {PA_15, GPIOA, GPIO_PIN_15};	//	E
+gpio_pin_t led5 = {PA_8, GPIOA, GPIO_PIN_8};		//	F
+gpio_pin_t led6 = {PI_3, GPIOI, GPIO_PIN_3};		//	G
+
+gpio_pin_t but = {PB_8, GPIOB, GPIO_PIN_8};		//	BUTTON
+
+int prnd; // Store random number
 
 // this is the main method
 int main()
@@ -23,17 +49,72 @@ int main()
   // properly
   HAL_Init();
   init_sysclk_216MHz();
+	init_random();
   
-  // initialise the gpio pin
-  init_gpio(led, OUTPUT);
+  // initialise the gpio pins
+  init_gpio(led0, OUTPUT);
+	init_gpio(led1, OUTPUT);
+	init_gpio(led2, OUTPUT);
+	init_gpio(led3, OUTPUT);
+	init_gpio(led4, OUTPUT);
+	init_gpio(led5, OUTPUT);
+	init_gpio(led6, OUTPUT);
+	init_gpio(but, INPUT);
   
   // loop forever ...
   while(1)
   {
-    // toggle the led on the gpio pin
-    toggle_gpio(led);
-    
-    // wait for 1 second
-    HAL_Delay(1000);
+		// RESET ALL PINS
+		write_gpio(led0, LOW);
+		write_gpio(led1, LOW);
+		write_gpio(led2, LOW);
+		write_gpio(led3, LOW);
+		write_gpio(led4, LOW);
+		write_gpio(led5, LOW);
+		write_gpio(led6, LOW);
+		
+		//	generate random number
+		uint32_t rnd = (get_random_int() % 6) + 1;
+		
+		if (prnd == rnd) {
+			uint32_t rnd = (get_random_int() % 6) + 1;
+		}
+		else {
+			if (rnd == 1){
+				write_gpio(led3, HIGH);
+			}
+			if (rnd == 2){
+				write_gpio(led0, HIGH);
+				write_gpio(led6, HIGH);
+			}
+			if (rnd == 3){
+				write_gpio(led0, HIGH);
+				write_gpio(led3, HIGH);
+				write_gpio(led6, HIGH);
+			}
+			if (rnd == 4){
+				write_gpio(led0, HIGH);
+				write_gpio(led1, HIGH);
+				write_gpio(led5, HIGH);
+				write_gpio(led6, HIGH);
+			}
+			if (rnd == 5){
+				write_gpio(led0, HIGH);
+				write_gpio(led1, HIGH);
+				write_gpio(led3, HIGH);
+				write_gpio(led5, HIGH);
+				write_gpio(led6, HIGH);
+			}
+			if (rnd == 6){
+				write_gpio(led0, HIGH);
+				write_gpio(led1, HIGH);
+				write_gpio(led2, HIGH);
+				write_gpio(led4, HIGH);
+				write_gpio(led5, HIGH);
+				write_gpio(led6, HIGH);
+			}
+		}
+		prnd = rnd;
+		HAL_Delay(1000);
   }
 }
