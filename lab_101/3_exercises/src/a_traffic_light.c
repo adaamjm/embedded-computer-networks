b54507168fdb88f0b71e4c95e5308afb677b2b08
adaamjm <adaamjm@users.noreply.github.com>
2017-09-28 12:52:52 +0100

    Task3a

--- lab_101/3_exercises/src/a_traffic_light.c
+++ lab_101/3_exercises/src/a_traffic_light.c
@@ -14,7 +14,9 @@
 #include "gpio.h"
 
 // map the led to GPIO PA8
-gpio_pin_t led = {PI_1, GPIOI, GPIO_PIN_1};
+gpio_pin_t led0 = {PI_1, GPIOI, GPIO_PIN_1};
+gpio_pin_t led1 = {PB_14, GPIOB, GPIO_PIN_14};
+gpio_pin_t led2 = {PB_15, GPIOB, GPIO_PIN_15};
 
 // this is the main method
 int main()
@@ -25,15 +27,28 @@ int main()
   init_sysclk_216MHz();
   
   // initialise the gpio pin
-  init_gpio(led, OUTPUT);
+  init_gpio(led0, OUTPUT);
+	init_gpio(led1, OUTPUT);
+	init_gpio(led2, OUTPUT);
   
   // loop forever ...
   while(1)
   {
     // toggle the led on the gpio pin
-    toggle_gpio(led);
-    
-    // wait for 1 second
-    HAL_Delay(1000);
+    toggle_gpio(led0);	//	RED ON
+		HAL_Delay(2000);		//	WAIT 2 SECONDS
+		toggle_gpio(led1);	//	ORANGE ON
+		HAL_Delay(2000);		//	WAIT 2 SECONDS
+		toggle_gpio(led0);	//	RED OFF
+		toggle_gpio(led1);	//	ORANGE OFF
+		toggle_gpio(led2);	//	GREEN ON
+		HAL_Delay(10000);		//	WAIT 10 SECONDS
+		toggle_gpio(led2);	//	GREEN OFF
+		toggle_gpio(led1);	//	ORANGE ON
+		HAL_Delay(2000);		//	WAIT 2 SECONDS
+		toggle_gpio(led0);	//	RED ON
+		toggle_gpio(led1);	//	ORANGE OFF
+		HAL_Delay(2000);		//	WAIT 2 SECONDS
+		toggle_gpio(led0);	//	RED OFF
   }
 }
